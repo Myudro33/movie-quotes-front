228ff2797a9358a6908ee4263a6dcb8a52b9294a
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-25 14:06:11 +0400

    style: add input and label components

---
+++ src/components/TheInput.vue
@@ -0,0 +1,41 @@
+<template>
+  <label-component :required="required" :label="id" :title="label" />
+  <Field
+    :class="`w-full h-[38px] ${
+      error ? 'border-red-500' : 'border-gray-500'
+    } px-5 border-2 text-lg mt-2 rounded-[4px]`"
+    :id="id"
+    :name="id"
+    :type="type"
+    :rules="rules"
+    :placeholder="placeholder"
+    :value="modelValue"
+    @input="changeValue"
+  />
+  <ErrorMessage class="text-[#F15524] xs:text-sm md:text-base" :name="id" />
+</template>
+
+<script>
+import { Field, ErrorMessage } from "vee-validate";
+export default {
+  props: [
+    "modelValue",
+    "id",
+    "placeholder",
+    "label",
+    "rules",
+    "required",
+    "type",
+    "error",
+  ],
+  components: {
+    Field,
+    ErrorMessage,
+  },
+  methods: {
+    changeValue($event) {
+      this.$emit("update:modelValue", $event.target.value);
+    },
+  },
+};
+</script>
---
+++ src/components/TheLabel.vue
@@ -0,0 +1,12 @@
+<template>
+  <label class="text-white font-bold flex" :for="label"
+    >{{ title }}
+    <p v-if="required" class="text-[#DC3545] ml-1">*</p></label
+  >
+</template>
+
+<script>
+export default {
+  props: ["label", "title", "required"],
+};
+</script>
