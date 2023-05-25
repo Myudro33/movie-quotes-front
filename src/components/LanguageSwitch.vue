12129a9b36febc3a72c6e4d27f01b6832ff379b7
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-25 14:04:35 +0400

    style: create language switch component

---
+++ src/components/LanguageSwitch.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="bg-transparent items-center xs:hidden md:flex">
+    <select
+      class="text-white bg-transparent active:outline-none text-xl"
+      v-model="$i18n.locale"
+      @change="changeLocale"
+    >
+      <option
+        class="text-black"
+        v-for="locale in $i18n.availableLocales"
+        :key="`locale-${locale}`"
+        :value="locale"
+      >
+        {{ locale === "en" ? "Eng" : "Geo" }}
+      </option>
+    </select>
+  </div>
+</template>
+
+//
+<script>
+import { setLocale } from "@vee-validate/i18n";
+export default {
+  methods: {
+    changeLocale($event) {
+      setLocale($event.target.value);
+      localStorage.setItem("locale", $event.target.value);
+    },
+  },
+  created() {
+    setLocale(localStorage.getItem("locale"));
+  },
+};
+</script>
