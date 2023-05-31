0b7c2e8358aca9f98469d33dbeb64395d2b99a5e
Myudro33 <nika.kanashvili@redberry.ge>
2023-05-31 19:41:50 +0400

    style: add logout button on navbar

--- src/components/TheNavbar.vue
+++ src/components/TheNavbar.vue
@@ -1,20 +1,32 @@
 <template>
-  <div class="xs:px-4 md:px-[70px] mt-6 flex justify-between items-center bg-transparent">
+  <div class="xs:px-4 md:px-[70px] h-20 flex justify-between items-center bg-[#181624]">
     <h1 class="text-[#DDCCAA] font-medium">MOVIE QUOTES</h1>
     <div class="flex xs:flex-row-reverse md:flex-row">
       <LanguageSwitch />
-      <button
-        @click="modalStore.openModal('register')"
-        class="bg-[#E31221] px-4 xs:h-8 md:h-[38px] text-white rounded-[4px] mx-4"
-      >
-        {{ $t("forms.sign_up") }}
-      </button>
-      <button
-        @click="modalStore.openModal('login')"
-        class="xs:w-[72px] xs:h-8 md:w-24 md:h-[38px] border border-[#fff] text-white rounded-[4px]"
-      >
-        {{ $t("forms.log_in") }}
-      </button>
+      <div v-if="!authStore.$state.user">
+        <button
+          @click="modalStore.openModal('register')"
+          class="bg-[#E31221] px-4 xs:h-8 md:h-[38px] text-white rounded-[4px] mx-4"
+        >
+          {{ $t("forms.sign_up") }}
+        </button>
+        <button
+          @click="modalStore.openModal('login')"
+          class="xs:w-[72px] xs:h-8 md:w-24 md:h-[38px] border border-[#fff] text-white rounded-[4px]"
+        >
+          {{ $t("forms.log_in") }}
+        </button>
+      </div>
+      <div v-else class="flex w-44 justify-evenly items-center">
+        <form @submit="authStore.logout()">
+          <button
+            type="submit"
+            class="text-white rounded-[4px] border border-white w-24 h-9"
+          >
+            Log out
+          </button>
+        </form>
+      </div>
     </div>
   </div>
 </template>
@@ -22,11 +34,13 @@
 <script>
 import LanguageSwitch from "./LanguageSwitch.vue";
 import { useModalStore } from "../stores/ModalStore";
+import { useAuthStore } from "../stores/AuthStore";
 export default {
   components: { LanguageSwitch },
   setup() {
     const modalStore = useModalStore();
-    return { modalStore };
+    const authStore = useAuthStore();
+    return { modalStore, authStore };
   },
 };
 </script>
