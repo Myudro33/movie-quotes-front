503d929c5afb0a2389ca909d309acb2688f7639d
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-06 13:34:29 +0400

    feat: add new localization titles

--- src/config/i18n/lang/en/index.js
+++ src/config/i18n/lang/en/index.js
@@ -2,12 +2,15 @@ import forms from './forms.json'
 import landing from './landing.json'
 import modal from './modal.json'
 import feed from './feed.json'
+import profile from './profile.json'
+
 
 const translations = {
   forms,
   landing,
   modal,
   feed,
+  profile
 }
 
 export default translations
--- src/router/index.js
+++ src/router/index.js
@@ -3,16 +3,21 @@ import { createRouter, createWebHistory } from 'vue-router'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
-   {
-    path:'/',
-    name:'landing',
-    component: ()=>import('../views/LandingPage.vue')
-   },
-   {
-    path:'/feed',
-    name:'feed',
-    component:()=>import('../views/FeedPage.vue')
-   }
+    {
+      path: '/',
+      name: 'landing',
+      component: () => import('../views/LandingPage.vue')
+    },
+    {
+      path:"/feed",
+      name: 'feed',
+      component: () => import('../views/FeedPage.vue'),
+      children:[
+        { path: 'news', name: 'news', component: () => import('../views/NewsFeed.vue') },
+        { path: 'edit-profile', name: 'editprofile', component: () => import('../views/ProfileEdit.vue') }
+      ]
+    },
+
   ]
 })
 
