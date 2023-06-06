112038bc2970fa016a4277ff804227973109357a
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-07 00:41:26 +0400

    style: create input for mobile device profile page

---
+++ src/components/MobileProfileInput.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <label-component :label="id" :title="label" />
+    <Field
+      :class="`w-full h-[38px] border-b ${
+        error ? 'border-b-red-500' : 'border-b-gray-500'
+      } px-5  text-lg mt-2 bg-transparent outline-none`"
+      :name="id"
+      :id="id"
+      :type="type"
+      :rules="rules"
+      :placeholder="placeholder"
+      :value="modelValue"
+      @input="changeValue"
+    />
+    <ErrorMessage class="text-[#F15524] xs:text-sm md:text-base" :name="id" />
+  </div>
+</template>
+
+<script>
+import { Field, ErrorMessage } from "vee-validate";
+export default {
+  props: ["modelValue", "id", "placeholder", "label", "rules", "type", "error"],
+  components: {
+    Field,
+    ErrorMessage,
+  },
+  methods: {
+    changeValue($event) {
+      this.$emit("update:modelValue", $event.target.value);
+    },
+  },
+};
+</script>
