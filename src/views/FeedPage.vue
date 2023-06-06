5e8e612d844f0a393f02a8d1c153d8e0b17133c3
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-06 13:36:52 +0400

    feat: modify feed page and add children routes

--- src/views/FeedPage.vue
+++ src/views/FeedPage.vue
@@ -2,30 +2,11 @@
   <div class="flex md:mt-8">
     <TheSidebar />
     <div class="mx-auto xs:w-full md:w-[938px]">
-      <div class="flex">
-        <button
-          class="bg-[#24222F] text-white rounded-[10px] md:w-auto md:h-[52px] xs:w-full xs:h-[86px] text-xl font-normal flex items-center px-3"
-        >
-          <PencilIcon />
-          {{ $t("feed.write_new_quote") }}
-        </button>
-        <div
-          class="ml-8 xs:hidden md:flex items-center h-[52px] border-b border-[#EFEFEF4D]"
-        >
-          <SearchIcon />
-          <input
-            class="w-[493px] h-[30px] bg-transparent text-white px-3"
-            :placeholder="$t('feed.search_placeholder')"
-            type="text"
-          />
-        </div>
-      </div>
+      <RouterView></RouterView>
     </div>
   </div>
 </template>
 
 <script setup>
 import TheSidebar from "../components/TheSidebar.vue";
-import PencilIcon from "../components/icons/PencilIcon.vue";
-import SearchIcon from "../components/icons/SearchIcon.vue";
 </script>
