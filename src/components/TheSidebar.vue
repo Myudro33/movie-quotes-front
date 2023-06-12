9c84e0dd26fde71c29f2581e8ea08bc9c49a2cdd
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-12 13:36:08 +0400

    refactor: remove duplicates from sidebar component

--- src/components/TheSidebar.vue
+++ src/components/TheSidebar.vue
@@ -1,20 +1,33 @@
 <template>
-  <!-- large screens -->
-  <div class="ml-[70px] w-72 lg:flex flex-col xs:hidden fixed">
-    <div class="flex">
-      <img
-        class="w-[60px] h-[60px] object-cover rounded-full"
-        :src="feed.avatar"
-        alt="avatar"
-      />
-      <div class="ml-6">
-        <h1 v-if="authStore.author" class="text-2xl text-white font-normal">
-          {{ authStore.author.username }}
-        </h1>
-        <a href="/feed/edit-profile" class="text-[#CED4DA]">{{
-          $t("feed.edit_profile")
-        }}</a>
+  <div
+    :class="`md:ml-[70px] xs:h-full md:h-auto xs:w-full md:w-72 xs:bg-[#11101A] md:bg-transparent xs:p-6 md:p-0  z-30 md:flex flex-col xs:${
+      sidebarStore.isHidden ? 'hidden' : 'flex'
+    } xs:absolute md:fixed`"
+  >
+    <LanguageSwitch class="md:hidden" />
+    <div class="flex justify-between">
+      <div class="flex">
+        <img
+          class="w-[60px] h-[60px] object-cover rounded-full"
+          :src="feed.avatar"
+          alt="avatar"
+        />
+        <div class="ml-6">
+          <h1 v-if="authStore.author" class="text-2xl text-white font-normal">
+            {{ authStore.author.username }}
+          </h1>
+          <a href="/feed/edit-profile" class="text-[#CED4DA]">{{
+            $t("feed.edit_profile")
+          }}</a>
+        </div>
       </div>
+      <button
+        @click="authStore.logout()"
+        type="submit"
+        class="text-white md:hidden rounded-[4px] border border-white w-24 h-9"
+      >
+        {{ $t("forms.log_out") }}
+      </button>
     </div>
     <a href="/feed/">
       <div class="mt-10 px-3 flex items-center">
@@ -24,52 +37,6 @@
     </a>
     <a href="/feed/films">
       <div class="mt-10 px-3 flex items-center">
-        <CameraIcon :color="feed.route === '/feed/films' ? '#E31221' : '#fff'" />
-        <h1 class="ml-11 text-2xl text-white">{{ $t("feed.movie_list") }}</h1>
-      </div>
-    </a>
-  </div>
-  <!-- small screen -->
-  <div
-    :hidden="sidebarStore.isHidden"
-    class="md:hidden w-full h-full bg-[#11101A] absolute z-30 p-8"
-  >
-    <LanguageSwitch />
-    <div class="flex items-center">
-      <img
-        class="w-10 h-10 rounded-full object-cover shrink-0"
-        :src="feed.avatar"
-        alt="avatar"
-      />
-      <div class="ml-6 w-full flex justify-between">
-        <div>
-          <h1 v-if="authStore.author" class="text-2xl text-white font-normal">
-            {{ authStore.author.username }}
-          </h1>
-          <a
-            @click="sidebarStore.isHidden = true"
-            href="/feed/edit-profile"
-            class="text-[#CED4DA]"
-            >{{ $t("feed.edit_profile") }}</a
-          >
-        </div>
-        <button
-          @click="authStore.logout()"
-          type="submit"
-          class="text-white rounded-[4px] border border-white w-24 h-9"
-        >
-          {{ $t("forms.log_out") }}
-        </button>
-      </div>
-    </div>
-    <a @click="sidebarStore.isHidden = true" href="/feed/">
-      <div class="mt-10 flex items-center">
-        <HouseIcon :color="feed.route === '/feed/' ? '#E31221' : '#fff'" />
-        <h1 class="ml-11 text-2xl text-white">{{ $t("feed.news_feed") }}</h1>
-      </div>
-    </a>
-    <a @click="sidebarStore.isHidden = true" href="/feed/films">
-      <div class="mt-10 flex items-center">
         <CameraIcon :color="feed.route === '/feed/films' ? '#E31221' : '#fff'" />
         <h1 class="ml-11 text-2xl text-white">{{ $t("feed.movie_list") }}</h1>
       </div>
