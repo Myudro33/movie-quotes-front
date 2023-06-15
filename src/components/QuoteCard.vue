fe59c80f1d68bd150028d736f087a10b30636f90
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-15 15:42:37 +0400

    feat: render dynamic quote data

--- src/components/QuoteCard.vue
+++ src/components/QuoteCard.vue
@@ -1,26 +1,23 @@
-import CommentIcon from './icons/CommentIcon.vue';
-
 <template>
   <div class="w-full flex flex-col bg-[#11101A] rounded-xl md:p-6 xs:p-8 mt-6">
     <div class="flex items-center">
       <img
         class="xs:w-10 xs:h-10 md:w-[52px] md:h-[52px] object-cover rounded-full"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLAU3eZR3MYzpxRl4lBB40Pmx3gBefdpCArQ&usqp=CAU"
+        :src="quote.user?.avatar"
         alt="avatar"
       />
-      <h1 class="text-white md:text-xl ml-3">Maia Nakashidze</h1>
+      <h1 class="text-white md:text-xl ml-3">{{ quote.user?.username }}</h1>
     </div>
     <p class="text-white md:text-xl mt-4">
-      “Follow your dream.”movie- Billy Elliot. (2000)
+      “{{ quote.title?.en }}”
+      <span class="text-[#DDCCAA] cursor-pointer">{{ quote.movie?.name.en }}</span> ({{
+        quote.movie?.year
+      }})
     </p>
-    <img
-      class="w-full md:h-[500px] xs:h-48 mt-7"
-      src="https://media.istockphoto.com/id/1450589820/photo/group-of-people-watching-a-movie-outdoors.webp?b=1&s=170667a&w=0&k=20&c=yuORtZmxMDC1IDmuYQZHpLNuOemOJ2iBXd5-rhYx6Gs="
-      alt="quote"
-    />
+    <img class="w-full md:h-[500px] xs:h-48 mt-7" :src="quote.image" alt="quote" />
     <div class="flex xs:h-16 md:h-20 items-center border-b border-b-[#EFEFEF4D]">
       <div class="flex">
-        <p class="text-white text-xl mr-2">3</p>
+        <p class="text-white text-xl mr-2">{{ quote?.comments.length }}</p>
         <CommentIcon />
       </div>
       <div class="flex ml-6">
@@ -28,34 +25,7 @@ import CommentIcon from './icons/CommentIcon.vue';
         <HeartIcon />
       </div>
     </div>
-    <div class="flex xs:flex-col md:flex-row mt-6 pb-6 border-b border-b-[#EFEFEF4D]">
-      <img
-        class="xs:w-10 xs:h-10 md:w-[52px] md:h-[52px] object-cover shrink-0 rounded-full"
-        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQLAU3eZR3MYzpxRl4lBB40Pmx3gBefdpCArQ&usqp=CAU"
-        alt="avatar"
-      />
-      <div class="md:ml-6">
-        <h1 class="text-white md:text-xl mt-3 font-semibold">Maia Nakashidze</h1>
-        <p class="text-white md:text-xl mt-3">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque nunc vel
-          massa facilisis consequat elit morbi convallis convallis. Volutpat vitae et nisl
-          et. Adipiscing enim integer mi leo nisl. Arcu vitae mauris odio eget.
-        </p>
-      </div>
-    </div>
-    <div class="flex xs:flex-col md:flex-row mt-6 pb-6 border-b border-b-[#EFEFEF4D]">
-      <img
-        class="xs:w-10 xs:h-10 md:w-[52px] md:h-[52px] object-cover shrink-0 rounded-full"
-        :src="authStore.author.avatar"
-        alt="avatar"
-      />
-      <div class="md:ml-6">
-        <h1 class="text-white md:text-xl mt-3 font-semibold">Maia Nakashidze</h1>
-        <p class="text-white md:text-xl mt-3">
-          Lorem ipsum dolor sit at moArcu vitae mauris odio eget.
-        </p>
-      </div>
-    </div>
+    <TheComment v-for="(item, index) in quote?.comments" :comment="item" :key="index" />
     <div class="flex items-center mt-6">
       <img
         class="xs:w-10 xs:h-10 md:w-[52px] md:h-[52px] object-cover text-white shrink-0 rounded-full"
@@ -75,5 +45,7 @@ import CommentIcon from './icons/CommentIcon.vue';
 import { useAuthStore } from "../stores/AuthStore";
 import CommentIcon from "./icons/CommentIcon.vue";
 import HeartIcon from "./icons/HeartIcon.vue";
+import TheComment from "./TheComment.vue";
+const props = defineProps(["quote"]);
 const authStore = useAuthStore();
 </script>
