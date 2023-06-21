6756b5777a063ac606f96557e4bd3a450907bedd
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-21 14:59:44 +0400

    fix: google auth

--- src/router/index.js
+++ src/router/index.js
@@ -11,6 +11,7 @@ const router = createRouter({
       meta: { auth: false },
     },
     {path:'/:token', meta:{auth:false}, name:'verify',component:()=>import('../views/TokenVerify.vue')},
+    {path:'/auth/google/callback', meta:{auth:false}, name:'verify',component:()=>import('../views/Google.vue')},
     {
       path: "/feed",
       name: 'feed',
