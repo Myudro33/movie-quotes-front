2bf7c2d40ad60e5c460feda41487b4118280d228
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-22 22:04:55 +0400

    style: create author tag component

---
+++ src/components/AuthorTag.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="flex md:mt-7 items-center">
+    <img
+      class="xs:w-10 xs:h-10 md:w-16 md:h-16 rounded-full object-cover"
+      :src="AuthStore.author.avatar"
+      alt="avatar"
+    />
+    <h1 class="text-white text-lg ml-4">{{ AuthStore.author.username }}</h1>
+  </div>
+</template>
+
+<script setup>
+import { useAuthStore } from "../stores/AuthStore";
+const AuthStore = useAuthStore();
+</script>
