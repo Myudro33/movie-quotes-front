b34e07e8429665dbea9c53464bcc1093fff61d67
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-22 22:07:19 +0400

    fix: remove scroll lock on modal wrapper component

--- src/components/ModalWrapper.vue
+++ src/components/ModalWrapper.vue
@@ -1,7 +1,7 @@
 <template>
   <div
     @click="closeModal"
-    class="absolute w-full h-full flex justify-center bg-[#18162380] left-0 top-0 z-[100]"
+    class="absolute w-full min-h-screen flex justify-center bg-[#18162380] left-0 top-0 z-30"
   >
     <div @click.stop>
       <slot></slot>
@@ -11,19 +11,8 @@
 
 <script setup>
 import { useNewsStore } from "../stores/NewsStore";
-import { useModalStore } from "../stores/ModalStore";
-import { onMounted, onBeforeUnmount } from "vue";
 const NewsStore = useNewsStore();
-const modalStore = useModalStore();
 const closeModal = () => {
   NewsStore.modal = "";
 };
-onMounted(() => {
-  if (NewsStore.modal !== "") {
-    modalStore.scroll(true);
-  }
-});
-onBeforeUnmount(() => {
-  modalStore.scroll(false);
-});
 </script>
