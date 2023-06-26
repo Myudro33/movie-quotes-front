39a7eac37192b5cb238a0f439966d0c4385755e2
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-26 13:06:14 +0400

    refactor: move  add genres function in services

--- src/services/index.js
+++ src/services/index.js
@@ -1,6 +1,27 @@
 import { computed } from "vue";
 import { useAuthStore } from "../stores/AuthStore";
-const AuthStore =useAuthStore()
-export const avatar =computed(()=>{
+const AuthStore = useAuthStore()
+export const avatar = computed(() => {
     return import.meta.env.VITE_API_IMAGE_ENDPOINT + AuthStore.author.avatar;
 })
+
+export const newGenre = (chips, value, props, editChips) => {
+    if (!props.edit) {
+        if (chips.value.includes(value)) {
+            return;
+        } else {
+            chips.value.push(value);
+        }
+    } else {
+        if (editChips.value.includes(value)) {
+            return;
+        } else {
+            if (chips.value.includes(value)) {
+                return;
+            } else {
+                chips.value.push(value);
+            }
+            editChips.value.push(value);
+        }
+    }
+}
