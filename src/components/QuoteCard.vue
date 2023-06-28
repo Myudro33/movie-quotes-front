1ffa3e03647db59cbb12205212f82aff4e1e9ebe
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-28 21:19:45 +0400

    refactor: move like functions into likeservice

--- src/components/QuoteCard.vue
+++ src/components/QuoteCard.vue
@@ -13,7 +13,11 @@
       <span class="text-[#DDCCAA] cursor-pointer">{{ quote.movie.name[locale] }}</span>
       ({{ quote.movie.year }})
     </p>
-    <img class="w-full md:h-[500px] xs:h-48 mt-7" :src="image + quote.image" alt="quote" />
+    <img
+      class="w-full md:h-[500px] xs:h-48 mt-7"
+      :src="image + quote.image"
+      alt="quote"
+    />
     <div class="flex xs:h-16 md:h-20 items-center border-b border-b-[#EFEFEF4D]">
       <div class="flex">
         <p class="text-white text-xl mr-2">{{ quote?.comments?.length }}</p>
@@ -27,8 +31,12 @@
       </div>
     </div>
     <TheComment v-for="(item, index) in limitedComments" :comment="item" :key="index" />
-    <button v-if="comments.length > 2" @click="toggleComments" class="text-white my-2 font-bold">
-      {{ showMoreButton ? $t('addquote.show_more') : $t('addquote.show_less') }}
+    <button
+      v-if="comments.length > 2"
+      @click="toggleComments"
+      class="text-white my-2 font-bold"
+    >
+      {{ showMoreButton ? $t("addquote.show_more") : $t("addquote.show_less") }}
     </button>
     <Form @submit="addComment" class="flex flex-col mt-6">
       <div class="w-full flex">
@@ -52,55 +60,56 @@
 </template>
 
 <script setup>
-import { Form, Field, ErrorMessage } from 'vee-validate'
-import { useAuthStore } from '../stores/AuthStore'
-import { useNewsStore } from '../stores/NewsStore'
-import CommentIcon from './icons/CommentIcon.vue'
-import HeartIcon from './icons/HeartIcon.vue'
-import TheComment from './TheComment.vue'
-import { computed, ref } from 'vue'
-import { useI18n } from 'vue-i18n'
-import { avatar, image, likeService } from '../services/index.js'
-const AuthStore = useAuthStore()
-const NewsStore = useNewsStore()
-const props = defineProps(['quote'])
+import { Form, Field, ErrorMessage } from "vee-validate";
+import { useAuthStore } from "../stores/AuthStore";
+import { useNewsStore } from "../stores/NewsStore";
+import CommentIcon from "./icons/CommentIcon.vue";
+import HeartIcon from "./icons/HeartIcon.vue";
+import TheComment from "./TheComment.vue";
+import { computed, ref } from "vue";
+import { useI18n } from "vue-i18n";
+import { avatar, image } from "../services/index.js";
+import { likeService } from "../services/likeService.js";
+const AuthStore = useAuthStore();
+const NewsStore = useNewsStore();
+const props = defineProps(["quote"]);
 const locale = computed(() => {
-  return useI18n().locale.value
-})
-const title = ref('')
-const comments = ref(props.quote.comments)
-const commentsToShow = ref(2)
+  return useI18n().locale.value;
+});
+const title = ref("");
+const comments = ref(props.quote.comments);
+const commentsToShow = ref(2);
 const showMoreButton = computed(() => {
-  return comments.value.length > commentsToShow.value
-})
+  return comments.value.length > commentsToShow.value;
+});
 const limitedComments = computed(() => {
-  return comments.value.slice(0, commentsToShow.value)
-})
+  return comments.value.slice(0, commentsToShow.value);
+});
 const toggleComments = () => {
   if (showMoreButton.value) {
-    commentsToShow.value = comments.value.length
+    commentsToShow.value = comments.value.length;
   } else {
-    commentsToShow.value = 2
+    commentsToShow.value = 2;
   }
-}
+};
 const liked = computed(() => {
-  return props.quote?.likes.some((like) => like.author_id === AuthStore.author.id)
-})
+  return props.quote?.likes.some((like) => like.author_id === AuthStore.author.id);
+});
 const addComment = () => {
   NewsStore.comment({
     user_id: AuthStore.author.id,
     quote_id: props.quote.id,
-    title: title.value
-  })
-  title.value = ''
-}
+    title: title.value,
+  });
+  title.value = "";
+};
 const addLike = async () => {
   likeService(
     {
       quote_id: props.quote.id,
-      user_id: AuthStore.author.id
+      user_id: AuthStore.author.id,
     },
     liked
-  )
-}
+  );
+};
 </script>
--- src/services/index.js
+++ src/services/index.js
@@ -2,8 +2,7 @@ import { computed } from 'vue'
 import { useModalStore } from '../stores/ModalStore.js'
 import router from '../router'
 import axiosInstance from '../config/axios-config'
-import { useNewsStore } from '../stores/NewsStore.js'
-import { useAuthStore } from '../stores/AuthStore.js'
+
 export const avatar = computed(() => {
   return import.meta.env.VITE_API_IMAGE_ENDPOINT + 'avatars/'
 })
@@ -33,29 +32,4 @@ export const verify = async (url) => {
         router.push({ name: 'news' })
       })
   }
-}
-
-export const likeService = async (data, liked) => {
-  if (liked.value) {
-    deleteLike(data)
-  } else {
-    addLike(data)
-  }
-}
-
-const deleteLike = async (data) => {
-  const AuthStore = useAuthStore()
-  const NewsStore = useNewsStore()
-  const quote = NewsStore.quotes.find((quote) => quote.id === data.quote_id)
-  const like = quote.likes.find((like) => like.author_id === AuthStore.author.id)
-  await axiosInstance.delete(`/likes/${like.id}`)
-  const filtered = quote.likes.filter((like) => like.author_id !== AuthStore.author.id)
-  return (quote.likes = filtered)
-}
-
-const addLike = async (data) => {
-  const NewsStore = useNewsStore()
-  const response = await axiosInstance.post('/likes', data)
-  const quote = NewsStore.quotes.find((quote) => quote.id === response.data.like.quote_id)
-  return quote.likes.push(response.data.like)
 }
