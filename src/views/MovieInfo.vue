5e3cb22bb87fa61757542295ba8a0a71006c80fe
Myudro33 <nika.kanashvili@redberry.ge>
2023-06-29 14:46:42 +0400

    style: add modal for movie inner page quotes

--- src/views/MovieInfo.vue
+++ src/views/MovieInfo.vue
@@ -1,12 +1,17 @@
 <template>
-  <AddQuoteModal :inner="true" :movie="MovieStore.movie" />
+  <AddQuoteModal
+    v-if="NewsStore.modal === 'add-quote'"
+    :inner="true"
+    :stage="quoteModalStage"
+    :movie="MovieStore.movie"
+  />
   <AddMovieModal :edit="true" v-if="MovieStore.modal === 'add-movie'" />
   <h1 v-if="loading"></h1>
   <div v-else class="flex flex-col absolute xs:py-10 md:py-0">
     <h1 class="text-2xl text-white xs:hidden md:flex">
       {{ $t("add_movie.movie_description") }}
     </h1>
-    <div class="flex mt-8 xs:flex-col md:flex-row xs:px-9 md:px-0">
+    <div class="flex mt-8 xs:flex-col md:flex-row xs:px-8 md:px-0">
       <img
         class="xs:w-full xs:h-72 md:w-[50rem] md:h-[27rem] rounded-xl shrink-0"
         :src="image + MovieStore.movie.image"
@@ -70,46 +75,79 @@
       </button>
     </div>
     <div
-      class="md:w-[50rem] md:h-[17rem] bg-[#11101A] mt-10 py-6 px-8 flex flex-col md:rounded-xl"
-      v-for="(item, index) in MovieStore.movie.quotes"
-      :key="index"
+      class="md:w-[61rem] relative"
+      v-for="item in MovieStore.movie.quotes"
+      :key="item.id"
     >
-      <div class="flex xs:flex-col md:flex-row md:justify-between md:items-center">
-        <img
-          class="shrink-0 xs:w-full md:w-56 h-36 rounded-sm"
-          :src="image + item.image"
-          alt="quote"
-        />
-        <h1 class="text-[#CED4DA] xs:my-3 md:my-0 text-2xl">
-          "{{ locale === "en" ? item.title.en : item.title.ka }}"
-        </h1>
-        <DotsIcon class="md:-translate-y-16 xs:hidden md:flex" />
-      </div>
-      <hr class="border w-full border-[#EFEFEF33] mt-6" />
-      <div class="flex mt-4 justify-between">
-        <div class="flex">
-          <div class="flex">
-            <h1 class="text-white text-2xl mr-2">{{ item.comments?.length }}</h1>
-            <CommentIcon />
+      <div
+        class="md:w-[50rem] md:h-[17rem] bg-[#11101A] mt-10 py-6 px-8 flex flex-col md:rounded-xl"
+      >
+        <div
+          v-if="quoteModal === item.id"
+          class="w-60 h-48 flex flex-col text-white justify-between absolute xs:right-4 md:right-0 xs:bottom-16 md:top-24 py-8 px-6 bg-[#24222F] rounded-lg"
+        >
+          <div
+            class="flex items-center cursor-pointer"
+            @click="setStage('view', item.id)"
+          >
+            <EyeIcon2 />
+            <h1 class="ml-2">{{ $t("add_quote.view_quote") }}</h1>
           </div>
-          <div class="flex">
-            <h1 class="text-white text-2xl ml-4 mr-2">{{ item.likes?.length }}</h1>
-            <HeartIcon :color="liked(item)" @click="addLike(item)" />
+          <div
+            class="flex items-center cursor-pointer"
+            @click="setStage('edit', item.id)"
+          >
+            <PenIcon />
+            <h1 class="ml-2">{{ $t("add_quote.edit_quote") }}</h1>
+          </div>
+          <div class="flex items-center cursor-pointer">
+            <TrashIcon />
+            <h1 class="ml-2">{{ $t("add_quote.delete_quote") }}</h1>
           </div>
         </div>
-        <DotsIcon class="md:hidden" />
+        <div class="flex xs:flex-col md:flex-row md:justify-between md:items-center">
+          <img
+            class="shrink-0 xs:w-full md:w-56 h-36 rounded-sm"
+            :src="image + item.image"
+            alt="quote"
+          />
+          <h1 class="text-[#CED4DA] xs:my-3 md:my-0 text-2xl">
+            "{{ locale === "en" ? item.title.en : item.title.ka }}"
+          </h1>
+          <DotsIcon
+            @click="modal(item.id)"
+            class="md:-translate-y-16 cursor-pointer xs:hidden md:flex"
+          />
+        </div>
+        <hr class="border w-full border-[#EFEFEF33] mt-6" />
+        <div class="flex mt-4 justify-between">
+          <div class="flex">
+            <div class="flex">
+              <h1 class="text-white text-2xl mr-2">{{ item.comments?.length }}</h1>
+              <CommentIcon />
+            </div>
+            <div class="flex">
+              <h1 class="text-white text-2xl ml-4 mr-2">{{ item.likes?.length }}</h1>
+              <HeartIcon :color="liked(item)" @click="addLike(item)" />
+            </div>
+          </div>
+          <DotsIcon @click="modal(item.id)" class="md:hidden" />
+        </div>
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-import { image } from "../services";
+import { image, openQuoteModal } from "../services";
+import { modalStage } from "../services/quotesService";
 import { useI18n } from "vue-i18n";
 import { useNewsStore } from "../stores/NewsStore";
 import { useAuthStore } from "../stores/AuthStore";
 import { useMovieStore } from "../stores/MoviesStore";
 import { onMounted, computed, ref } from "vue";
+import { createLike, deleteLike } from "../services/likeService";
+import EyeIcon2 from "../components/icons/EyeIcon2.vue";
 import PenIcon from "../components/icons/PenIcon.vue";
 import TrashIcon from "../components/icons/TrashIcon.vue";
 import DotsIcon from "../components/icons/DotsIcon.vue";
@@ -118,6 +156,8 @@ import AddQuoteModal from "../components/AddQuoteModal.vue";
 import CommentIcon from "../components/icons/CommentIcon.vue";
 import HeartIcon from "../components/icons/HeartIcon.vue";
 import AddMovieModal from "../components/AddMovieModal.vue";
+const quoteModal = ref("");
+const quoteModalStage = ref("");
 const MovieStore = useMovieStore();
 const NewsStore = useNewsStore();
 const AuthStore = useAuthStore();
@@ -129,11 +169,11 @@ const locale = computed(() => {
   return useI18n().locale.value;
 });
 const addLike = (item) => {
-  MovieStore.addMovieQuoteLike({
+  const data = {
     quote_id: item.id,
     user_id: AuthStore.author.id,
-  });
-  liked(item);
+  };
+  liked(item) ? deleteLike(data, item) : createLike(data, item);
 };
 onMounted(async () => {
   await MovieStore.getMovie();
@@ -141,4 +181,12 @@ onMounted(async () => {
   await NewsStore.getQuotes();
   loading.value = false;
 });
+const modal = (num) => {
+  openQuoteModal(num, quoteModal);
+};
+const setStage = (val, index) => {
+  modalStage(val, quoteModalStage);
+  MovieStore.quote = MovieStore.movie.quotes.find((quote) => quote.id === index);
+  NewsStore.modal = "add-quote";
+};
 </script>
