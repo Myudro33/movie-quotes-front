750a093c68de0b6f68de9290709a8ee2d4bea04e
Myudro33 <nika.kanashvili@redberry.ge>
2023-07-02 14:58:05 +0400

    style: add not found page

---
+++ src/views/NotFound.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="w-full h-screen flex flex-col justify-center items-center text-white">
+    <GhostIcon />
+    <h1 class="text-2xl md:text-5xl h-16 font-bold mt-7">
+      {{ $t("not_found.heading") }}
+    </h1>
+    <p class="font-medium md:text-2xl mt-4">{{ $t("not_found.paragraph") }}</p>
+    <RouterLink :to="{ name: 'landing' }">
+      <button class="bg-[#E31221] px-4 py-2 rounded-md mt-10">
+        {{ $t("forbidden.return") }}
+      </button>
+    </RouterLink>
+  </div>
+</template>
+
+<script setup>
+import GhostIcon from "../components/icons/GhostIcon.vue";
+</script>
