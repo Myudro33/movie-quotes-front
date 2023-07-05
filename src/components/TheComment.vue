991744801fd7450a887fb5f87139deb5fd1b54cc
Myudro33 <nika.kanashvili@redberry.ge>
2023-07-05 22:16:37 +0400

    fix(style): comment component

--- src/components/TheComment.vue
+++ src/components/TheComment.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="flex xs:flex-col md:flex-row mt-6 pb-6 border-b border-b-[#EFEFEF4D]">
+  <div class="flex flex-row mt-6 pb-6 border-b border-b-[#EFEFEF4D]">
     <img
       class="xs:w-10 xs:h-10 md:w-[3.25rem] md:h-[3.25rem] object-cover shrink-0 rounded-full"
       :src="avatar + props.comment.author.avatar"
       alt="avatar"
     />
-    <div class="md:ml-6">
-      <h1 class="text-white md:text-xl mt-3 font-semibold">
+    <div class="xs:ml-2 md:ml-6">
+      <h1 class="text-white md:text-xl xs:mt-1 md:mt-3 font-semibold">
         {{ props.comment.author.username }}
       </h1>
       <p class="text-white md:text-xl mt-3">
